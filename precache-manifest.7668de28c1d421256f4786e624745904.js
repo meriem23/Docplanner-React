da8ea573e82d019dbf643e9aa82f5fbdccb1f7a6
meriem23 <esra.msallem@gmail.com>
2020-03-02 14:42:19 +0100

    Updates

---
+++ precache-manifest.7668de28c1d421256f4786e624745904.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62ffc87d7bdab0f97dda2c36eb79fd98",
+    "url": "/Docplanner-React/index.html"
+  },
+  {
+    "revision": "144e2ecca542f72126ba",
+    "url": "/Docplanner-React/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8232d581d94a1ef7922a",
+    "url": "/Docplanner-React/static/css/main.3669360f.chunk.css"
+  },
+  {
+    "revision": "144e2ecca542f72126ba",
+    "url": "/Docplanner-React/static/js/2.4e093f52.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/Docplanner-React/static/js/2.4e093f52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8232d581d94a1ef7922a",
+    "url": "/Docplanner-React/static/js/main.b2acfc18.chunk.js"
+  },
+  {
+    "revision": "46502f6d5a4f38e9cd4d",
+    "url": "/Docplanner-React/static/js/runtime-main.dbf6aeb7.js"
+  }
+]);
